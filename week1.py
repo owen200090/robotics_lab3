6800e3cf14257a4f4fe516d24e8e23098c2d95da
owen200090 <61010245@kmitl.ac.th>
2020-01-28 17:03:06 +0700

    Fix

--- week1.py
+++ week1.py
@@ -1,8 +1,12 @@
-val = input("Put the sentence in: ")
-val = val.lower()
-output = []
-for char in val:
-    number = ord(char) - 96
-    output.append(number)
+def alphabet_position(val):
+    #val = input("Put the sentence in: ")
+    val = val.lower()
+    output = []
+    for char in val:
+        number = ord(char) - 96
+        if number != -64:
+            output.append(str(number))
+    return " ".join(output)
+
+print(alphabet_position("Superman"))
     
-print(output)

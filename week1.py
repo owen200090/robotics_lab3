d04ebce6139cd37381ca3018293982c8dc094901
owen200090 <61010245@kmitl.ac.th>
2020-02-02 18:06:42 +0700

    Fixed

--- week1.py
+++ week1.py
@@ -12,12 +12,3 @@
     #return " ".join(output).strip()
 
 #print(alphabet_position("The sunset o'clock"))
-    
-def validate_pin(x):
-    if len(x) != 4 and len(x) != 6:
-        return False
-    try:
-        int(x)
-        return True
-    except:
-        return False
--- week2.py
+++ week2.py
@@ -1,18 +1,3 @@
-#def alphabet_position(val):
-    #val = input("Put the sentence in: ")
- #   output = []
-  #  for char in val:
-   #     number = ord(char)
-    #    if 97 <= number and number <= 122:
-     #       number = number-96
-      #      output += str(number)+' '
-       # elif 65 <= number and number <= 90:
-        #    number = number-64
-         #   output += str(number)+' '
-    #return " ".join(output).strip()
-
-#print(alphabet_position("The sunset o'clock"))
-    
 def validate_pin(x):
     if len(x) != 4 and len(x) != 6:
         return False
@@ -21,3 +6,4 @@ def validate_pin(x):
         return True
     except:
         return False
+

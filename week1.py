9b6f88fbdb9778ab2bb8bcb60ec2bf2b3a444fbc
owen200090 <61010245@kmitl.ac.th>
2020-02-02 18:09:14 +0700

    Fixed

--- week1.py
+++ week1.py
@@ -1,14 +1,13 @@
-#def alphabet_position(val):
-    #val = input("Put the sentence in: ")
- #   output = []
-  #  for char in val:
-   #     number = ord(char)
-    #    if 97 <= number and number <= 122:
-     #       number = number-96
-      #      output += str(number)+' '
-       # elif 65 <= number and number <= 90:
-        #    number = number-64
-         #   output += str(number)+' '
-    #return " ".join(output).strip()
+def alphabet_position(val):
+    output = []
+    for char in val:
+        number = ord(char)
+        if 97 <= number and number <= 122:
+            number = number-96
+            output += str(number)+' '
+        elif 65 <= number and number <= 90:
+            number = number-64
+            output += str(number)+' '
+    return " ".join(output).strip()
 
-#print(alphabet_position("The sunset o'clock"))
+print(alphabet_position("The sunset o'clock"))
